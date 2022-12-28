e7b24f84d0ae2ce5b5d750b3d19f477fd779675c
Zhenxi <296488320@qq.com>
2022-12-28 16:50:12 +0800

    add get v4&v6

--- app/src/main/java/com/example/jnihook/MainActivity.java
+++ app/src/main/java/com/example/jnihook/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.jnihook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.provider.CallLog;
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class MainActivity extends Activity {
 
--- app/src/main/jni/main.cpp
+++ app/src/main/jni/main.cpp
@@ -90,29 +90,43 @@
 #include <netinet/ip.h>
 #include <netlink/ifaddrs.h>
 #include <bionic_netlink.h>
+#include <netdb.h>
 
 
-
-int listmacaddrs(void) {
+int listmacaddrs() {
     struct ifaddrs *ifap, *ifaptr;
 
     if (myGetifaddrs(&ifap) == 0) {
-        for (ifaptr = ifap; ifaptr != NULL; ifaptr = (ifaptr)->ifa_next) {
-            char macp[INET6_ADDRSTRLEN];
+        for (ifaptr = ifap; ifaptr != nullptr; ifaptr = (ifaptr)->ifa_next) {
             if(ifaptr->ifa_addr!= nullptr) {
-                if (((ifaptr)->ifa_addr)->sa_family == AF_PACKET) {
+                sa_family_t family = ((ifaptr)->ifa_addr)->sa_family;
+                if (family == AF_PACKET) {
+                    //get mac info
+                    char macp[INET6_ADDRSTRLEN];
                     auto *sockadd = (struct sockaddr_ll *) (ifaptr->ifa_addr);
                     int i;
                     int len = 0;
                     for (i = 0; i < 6; i++) {
                         len += sprintf(macp + len, "%02X%s", sockadd->sll_addr[i],( i < 5 ? ":" : ""));
                     }
-                    //LOGE("%s  %s  ",(ifaptr)->ifa_name,macp)
-                    if(strcmp(ifaptr->ifa_name,"wlan0")== 0){
-                        LOGE("%s  %s  ",(ifaptr)->ifa_name,macp)
-                        freeifaddrs(ifap);
-                        return 1;
+                    LOGE("AF_PACKET  %s  %s  ",(ifaptr)->ifa_name,macp)
+//                    if(strcmp(ifaptr->ifa_name,"wlan0")== 0){
+//                        LOGE("%s  %s  ",(ifaptr)->ifa_name,macp)
+//                        freeifaddrs(ifap);
+//                        return 1;
+//                    }
+                } else if(family == AF_INET || family == AF_INET6){
+                    //get v4 & v6 info
+                    char host[NI_MAXHOST];
+                    int ret = getnameinfo(ifaptr->ifa_addr,
+                                    (family == AF_INET) ? sizeof(struct sockaddr_in) :
+                                    sizeof(struct sockaddr_in6),
+                                    host, NI_MAXHOST,
+                                    nullptr, 0, NI_NUMERICHOST);
+                    if (ret != 0) {
+                        LOGE("AF_INET6  getnameinfo() failed   %s  ",gai_strerror(ret))
                     }
+                    LOGE("AF_INET6  %s  ",host)
                 }
             }
 
